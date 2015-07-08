d7517c68c0c2f8beaae11ede02f4b5a361e7cfa5
skornous <hugo.gennaro@gmail.com>
2015-07-08 12:42:54 +0200

    Updated UserTable to secure update, add delete and selectAll & change the getUser

--- src/Users/Model/UserTable.php
+++ src/Users/Model/UserTable.php
@@ -2,6 +2,7 @@
 
 namespace Users\Model;
 
+use Zend\Db\Sql\Select;
 use Zend\Db\TableGateway\TableGateway;
 
 class UserTable {
@@ -14,7 +15,9 @@ class UserTable {
 
 	function getUser($id) {
 		$id = (int) $id;
-		$rowSet = $this->tableGateway->select(array("id" => $id));
+		$select = new Select($this->tableGateway->getTable());
+		$select->columns(['id', 'name', 'email'])->where(['id' => $id]);
+		$rowSet = $this->tableGateway->selectWith($select);
 		$row = $rowSet->current();
 
 		if(!$row) {
@@ -28,19 +31,40 @@ class UserTable {
 		$data = [
 			"name" => $user->getName(),
 			"email" => $user->getEmail(),
-			"password" => $user->getPassword(),
 		];
-
 		$id = (int) $user->getId();
+
 		if($id === 0) {
+			$data['password'] = $user->getPassword();
 			$this->tableGateway->insert($data);
 		} else {
 			if ($this->getUser($id)) {
-				$this->tableGateway->update($data, array("id" => $id));
+				$this->tableGateway->update($data, ["id" => $id]);
 			} else {
 				throw new \Exception("User ID does not exists");
 			}
 		}
+	}
+
+	function fetchAll() {
+		$rowSet = $this->tableGateway->select();
+		return $rowSet;
+	}
+
+	function getUserByEmail($email) {
+		$rowSet = $this->tableGateway->select(array("email" => $email));
+		$row = $rowSet->current();
+
+		if(!$row) {
+			throw new \Exception("could not find row $email");
+		}
+
+		return $row;
+	}
+
+	function deleteUser($id) {
+		$id = (int) $id;
+		$this->tableGateway->delete(array("id" => $id));
 	}
 
 }
